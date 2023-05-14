ddc8e65546d957d09322ad23afc460943f392cd6
Adam <109966894+Siesta4@users.noreply.github.com>
2023-05-14 22:07:09 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+
+
+  // const array = [22, 0, -45, 1001, 8, -6, -0.1, 101, 2.5, -55555];
+    
+  // const positive = array.filter(test)
+  
+  // function test(item){
+  //   if(item > 0){
+  //   return item
+  //   }
+  // }
+
+
+
+
+
+
+
+  // const messages = [
+  //   {message: 'hello', error: true},  
+  //   {message: 'javascript', error: false},  
+  //   {message: 'expovisiov', error: true},  
+  //   {message: 'react', error: true},  
+  //   {message: 'angular', error: false}, 
+  //   {message: 'es6', error: false}, 
+  // ];
+  
+  // const filteredMessages = messages.filter(function(obj){
+  //   return obj.error === false;
+  // });
+
+
+
+
+
+
+
+
+  // const words = ['экспо', 'js', 'react', 'css', 'angular', 'vue', 'html'];
+
+  // const newWords = words.map(function(word){
+  //   if(word.length < 5) {
+  //     return '*';
+  //   }
+  //   return word;
+  // });
+  
+
+
+
+
+
+
+
+  // const sendMessage = (message) => {
+  //   /*
+  //   * тут какой-то большой код для отправки сообщения
+  //   */
+  // }
+  
+  // const half = number => number / 2;
+  
+  // const showConsole = () => console.log('Экспо');
+  
+  // const concatWords = (first, second) => first + second;
+
+
+
+
+
+
+
+  
+
+  // function justText() {
+  //   return 'expo';
+  // }
+  
+  // function logging(text) {
+  //   console.log(text);
+  // }
+  
+  // function add(x, y) {
+  //   const z = 3;
+  
+  //   return z * x * y;
+  // }
+  
+  // function onlyPositive(number) {
+  //   if(number < 0) {
+  //     return false;
+  //   }
+  
+  //   return true;
+  // } 
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {}
     </div>
   );
 }
